8ef8c10481a68abd7d059595c1d94f18bb4a4edb
Mark Madsen <mark@madsenlab.org>
2014-06-10 13:02:55 -0700

    Added a dummy module python file to keep setup.py happy, since it errors out during automated pip install in Travis.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup, Extension
 
 
-setup(name = "slatkin",
+setup(name = "slatkintools",
 	  version = '1.0',
 	  description = 'Python extension wrapping a modernized version of the "exact" test for neutrality by Montgomery Slatkin exact test',
 	  author = 'Mark E. Madsen (and Montgomery Slatkin)',
 	  author_email = "mark@madsenlab.org",
 	  url = "https://github.com/mmadsen/slatkin-exact-tools",
 	  ext_modules = [Extension('_slatkin', sources = ['slatkin.c', 'mersenne.c', 'slatkin.i'])],
-	  py_modules = ['slatkin']
+	  py_modules = ['slatkintools']
 	)
 
